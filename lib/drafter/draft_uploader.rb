4e8996f516feaccaa804ecd5d5efc6efefa00eae
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2012-08-16 22:08:46 +0100

    Docs on DraftUploader.

--- lib/drafter/draft_uploader.rb
+++ lib/drafter/draft_uploader.rb
@@ -1,3 +1,6 @@
+# This CarrierWave uploader is used to steal incoming files off of
+# draftable objects, and store them on a DraftUpload for later use.
+#
 class DraftUploader < CarrierWave::Uploader::Base
 
   storage :file
