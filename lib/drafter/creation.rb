b6bcf48fab2c3d62c9454c85501b09cfa7aa0c1a
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2012-08-16 20:32:20 +0100

    Refactoring and docs.

--- lib/drafter/creation.rb
+++ lib/drafter/creation.rb
@@ -15,26 +15,39 @@ module Drafter
       uploads = build_draft_uploads(attrs)
       self.build_draft(:data => attrs)
       self.draft.save!
-      puts "upload count: #{uploads.length}"
-      # debugger if uploads.length == 1
       self.draft.draft_uploads << uploads
       self.draft
     end
 
     private 
 
+      # Loop through and create DraftUpload objects for any Carrierwave
+      # uploaders mounted on this draftable object.
+      #
+      # @param [Hash] attrs the attributes to loop through
+      # @return [Array<DraftUpload>] an array of unsaved DraftUpload objects.
       def build_draft_uploads(attrs)
         draft_uploads = []
         attrs.keys.each do |key|
           if self.send(key).is_a?(CarrierWave::Uploader::Base)
-            cw_uploader = self.send(key)
-            file = File.new(cw_uploader.file.path)
-            draft_upload = DraftUpload.new(
-              :file_data => file, :draftable_mount_column => key)
-            draft_uploads << draft_upload
+            draft_uploads << build_draft_upload(key)
           end
         end
         draft_uploads
+      end
+
+      # Get a reference to the CarrierWave uploader mounted on the 
+      # current draftable object, grab the file in it, and shove
+      # that file into a new DraftUpload.
+      # 
+      # @param [String] key the attribute where the CarrierWave uploader 
+      #   is mounted. 
+      # @return [DraftUpload] containing the file in the uploader.
+      def build_draft_upload(key)
+        cw_uploader = self.send(key)
+        file = File.new(cw_uploader.file.path)
+        draft_upload = DraftUpload.new(
+          :file_data => file, :draftable_mount_column => key)
       end
 
   end
