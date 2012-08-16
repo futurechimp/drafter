1ac85ea30f441a5bd4bd9eff5ae5c519d56820de
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2012-08-16 19:09:54 +0100

    DraftUpload belongs to a draft.

--- lib/drafter/draft_upload.rb
+++ lib/drafter/draft_upload.rb
@@ -1,7 +1,15 @@
 class DraftUpload < ActiveRecord::Base
 
+	# Associations
+	#
+	belongs_to :draft
+
+	# Validations
+	#
 	validates_presence_of :original
 
+	# Macros
+	#
 	mount_uploader :original, DraftUploader
 
 end
--- test/drafter/test_draft_upload.rb
+++ test/drafter/test_draft_upload.rb
@@ -5,6 +5,7 @@ class TestDraftUpload < Minitest::Unit::TestCase
 	describe DraftUpload do
 		subject { DraftUpload.new }
 		it { must validate_presence_of :original }
+		it { must belong_to :draft }
 	end
 
 end
