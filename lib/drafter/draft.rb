3b00ac8d6c517fcd0b1660ca7cf98337fc7841e1
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2012-08-16 11:55:02 +0100

    Some comments.

--- lib/drafter/draft.rb
+++ lib/drafter/draft.rb
@@ -1,9 +1,17 @@
 class Draft < ActiveRecord::Base
 
+	# Validations
+	#
 	validates_presence_of :data
 
+	# Associations
+	#
 	belongs_to :draftable, :polymorphic => true
 
+
+	# Store serialized data for the associated draftable
+	# as a Hash of attributes.
+	#
   serialize :data, Hash
 
 end
