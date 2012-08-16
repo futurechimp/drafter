188352aec3c6e8190772c3e0636d124e3e55ce8b
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2012-08-16 20:36:28 +0100

    More testing of draft uploads, seems to work ok.

--- test/drafter/test_creation.rb
+++ test/drafter/test_creation.rb
@@ -33,17 +33,22 @@ class TestCreation < Minitest::Unit::TestCase
 
 			it "should create a DraftUpload object" do
 				assert_equal(@draft_upload_count + 1, DraftUpload.count)
-				# assert_equal "foo.txt", @draft.data["upload"]
+			end
+
+			it "should add the draft_upload to the Draft's array of them" do
+				assert_equal(1, @draft.draft_uploads.length)
+				assert_equal(DraftUpload.last, @draft.draft_uploads.first)
 			end
 		end
 
 		describe "an existing draftable article" do
 			before do
 				@draft_count = Draft.count
+				@draft_upload_count = DraftUpload.count
 				@article = Article.create!(:text => "original text")
 				@article.text = "draft text"
 				@article.upload = file_upload
-				@article.save_draft
+				@draft = @article.save_draft
 			end
 
 			describe "the article" do
@@ -65,9 +70,14 @@ class TestCreation < Minitest::Unit::TestCase
 					assert_equal('draft text', @article.draft.data["text"])
 				end
 
-				it "should save the contents of the CarrierWave attribute" do
-					# assert_equal "foo.txt", @article.draft.data["upload"]
-				end	
+				it "should create a DraftUpload object" do
+					assert_equal(@draft_upload_count + 1, DraftUpload.count)
+				end
+
+				it "should add the draft_upload to the Draft's array of them" do
+					assert_equal(1, @draft.draft_uploads.length)
+					assert_equal(DraftUpload.last, @draft.draft_uploads.first)
+				end
 			end
 		end
 	end
