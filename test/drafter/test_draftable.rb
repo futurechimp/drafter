96db4ed1875f6273d23b4bba20d1622b72d7c7b6
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2012-08-16 12:01:22 +0100

    A little more refactoring, quite a bit cleaner now.

---
+++ test/drafter/test_draftable.rb
@@ -0,0 +1,17 @@
+require 'helper'
+
+class TestDraftable < MiniTest::Unit::TestCase
+
+	describe "A draftable ActiveRecord class" do
+		it "should know it's draftable" do
+			assert Article.draftable?
+		end
+	end
+
+	describe "A non-draftable ActiveRecord class" do
+		it "should know it's not draftable" do
+			refute Comment.draftable?
+		end
+	end
+
+end
--- test/test_drafter.rb
+++ test/test_drafter.rb
@@ -1,19 +1,5 @@
 require 'helper'
 
 class TestDrafter < MiniTest::Unit::TestCase
-  
-	
-
-	describe "A draftable ActiveRecord class" do
-		it "should know it's draftable" do
-			assert Article.draftable?
-		end
-	end
-
-	describe "A non-draftable ActiveRecord class" do
-		it "should know it's not draftable" do
-			refute Comment.draftable?
-		end
-	end
 
 end
