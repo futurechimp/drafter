9f0a0fe4de772e93dc13fb9dd19efd0ab2266b83
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2012-08-16 19:37:29 +0100

    More docs.

--- lib/drafter/draft_upload.rb
+++ lib/drafter/draft_upload.rb
@@ -1,8 +1,12 @@
-# Some of the objects we want to keep drafts of will have attached
-# uploads, which are stored using CarrierWave. The simplest thing
-# which can work, I think, is to grab the uploaded data from the 
-# draftable object, and store it in its own class, making a note of
-# the original mount column that the file was destined for. 
+# Some of the objects we want to keep drafts of will have attached uploads, 
+# which are stored using CarrierWave. The simplest thing which can work, I 
+# think, is to grab the uploaded data from the draftable object, and store it 
+# in its own class, making a note of the original mount column that the file 
+# was destined for. 
+#
+# The original :file_data is saved in this model, without any processing
+# or modification. When the draft is approved, the Draft will push the
+# :file_data onto the :draftable_mount_column of the draftable object.
 #
 class DraftUpload < ActiveRecord::Base
 
