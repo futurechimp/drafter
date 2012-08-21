d1bb43fe6afc7f3da545faafe7336e710952c1b9
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2012-08-21 11:58:15 +0100

    Setting better storage paths.

--- lib/drafter/draft_uploader.rb
+++ lib/drafter/draft_uploader.rb
@@ -5,4 +5,18 @@ class DraftUploader < CarrierWave::Uploader::Base
 
   storage :file
 
+  ##
+  # Directory where uploaded files will be stored (default is /public/uploads)
+  #
+  def store_dir
+    "system/draft_uploads/#{model.to_param}"
+  end
+
+  ##
+  # Directory where uploaded temp files will be stored (default is [root]/tmp)
+  #
+  def cache_dir
+    "system/draft_uploads/tmp"
+  end
+
 end
