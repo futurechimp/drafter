d3d207ac5870701c59e4481d6bd5d1c2153edafa
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2012-08-29 11:18:48 +0100

    Committing a failing test as a reminder of what's wrong.

--- test/test_drafter.rb
+++ test/test_drafter.rb
@@ -2,4 +2,16 @@ require 'helper'
 
 class TestDrafter < MiniTest::Unit::TestCase
 
+  describe "A draftable class" do
+    it "responds to :save_draft" do
+      assert Article.new.respond_to?(:save_draft)
+    end
+  end
+
+  describe "A non-draftable class" do
+    it "does not respond to :save_draft" do
+      refute Comment.new.respond_to?(:save_draft)
+    end
+  end
+
 end
