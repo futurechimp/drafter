9e65bd79a1b6b04a09a4f60a65ba6c56dbb6640d
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2012-08-30 15:30:26 +0100

    Formatting.

--- test/support/models.rb
+++ test/support/models.rb
@@ -2,19 +2,14 @@
 
 # A draftable article class
 class Article < ActiveRecord::Base
-
   validates_presence_of :text
-
 	draftable
-
 	mount_uploader :upload, Uploader
-
 end
 
+# A simpler Post class
 class Post < ActiveRecord::Base
-
   draftable
-
 end
 
 
